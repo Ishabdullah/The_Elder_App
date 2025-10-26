298c31ba91523f7a44df2d6ada902958b08111f3
Ismail Abdullah <ismail.t.abdullah@gmail.com>
2025-10-26 09:51:44 -0400

    Auto-fix: namespace + Kotlin/KSP versions for GitHub build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.9.10"
+    kotlin("jvm") version "1.9.0"
     id("com.android.application") version "8.2.0"
-    id("org.jetbrains.kotlin.android") version "1.9.10"
+    id("org.jetbrains.kotlin.android") version "1.9.0"
     id("com.google.devtools.ksp") version "1.9.0-1.0.13"
 }
